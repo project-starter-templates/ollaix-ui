7fb26d37da5a693f4f7ef26d92a73b921b2de5d5
Macktireh <abdimack97@gmail.com>
2025-05-31 12:51:09 +0200

    chore: installing and configuring tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const { NODE_ENV } = loadEnv(process.env.NODE_ENV as string, process.cwd(), "");
 
@@ -13,7 +14,7 @@ export default defineConfig({
   preview: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
