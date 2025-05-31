8e731b16f0972868370edb350132f98ceb373482
Macktireh <abdimack97@gmail.com>
2025-05-31 15:05:11 +0200

    feat(routing): installing and configuring react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router";
+
 function App() {
-  return <div>Ollaix UI</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Hello World</div>} />
+      <Route path="/about" element={<div>about</div>} />
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
+  );
 }
 
 export default App;
